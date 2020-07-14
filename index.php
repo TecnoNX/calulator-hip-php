aa7facb64c3bfe1c9a241a1be43d5a4d249483c2
TecnoNX <40416331+TecnoNX@users.noreply.github.com>
2020-07-13 21:18:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="custom.css">
+    <title>Calculadora de hipotecaria</title>
+</head>
+<body>
+    <div class="form">
+    <h1>Calculadora hipotecaria.</h1>
+    <div class="camp">
+    
+    <form action="" method="post">
+    <h4>Valor del inmueble</h4>
+    <input type="text" name="" id="">
+
+    <h4>Pago Inicial</h4>
+    <input type="text" name="" id="">
+
+    <h4>Tasa de Interes (%)</h4>
+    <input type="text" name="" id="">
+
+    <h4>Periodo de amortizacion (meses)</h4>
+    <input type="text" name="" id="">
+
+    <input type="submit" value="Calcular">
+    </form>
+
+    </div>
+
+    </div>
+
+    <div class="resultados">
+    
+    </div>
+</body>
+</html>
